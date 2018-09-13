03d561263c4f40bf0246b0079479a0548795a38c
Xuijn <43236621+Xuijn@users.noreply.github.com>
2018-09-13 14:51:20 +0200

    Delete 4LetterWord.py

--- 4LetterWord.py
+++
@@ -1,5 +0,0 @@
-word = raw_input("Please enter a four-letter word: ")
-if len(word) == 4:
-    print word + " is a four-letter word. Careful now!"
-else:
-    print "That's not a four-letter word. Try harder, Noob."
