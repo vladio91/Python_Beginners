4ec578c6f4d78fd374b17c40657bc483d9922b2b
Xuijn <43236621+Xuijn@users.noreply.github.com>
2018-09-13 12:20:12 +0200

    Add files via upload

---
+++ 4LetterWord.py
@@ -0,0 +1,5 @@
+word = raw_input("Please enter a four-letter word: ")
+if len(word) == 4:
+    print word + " is a four-letter word. Careful now!"
+else:
+    print "That's not a four-letter word. Try harder, Noob."
